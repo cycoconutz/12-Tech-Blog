4f600cc457be4b8355da12024d957da270897ddc
John Yates <johndyates@gmail.com>
2022-05-08 14:19:00 -0500

    single post route written

--- Develop/controllers/home-routes.js
+++ Develop/controllers/home-routes.js
@@ -18,7 +18,18 @@ router.get('/', async (req, res) => {
 
 // get single post
 router.get('/post/:id', async (req, res) => {
-
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ all: true, nested: true }],
+    });
+    const post = postData.get({ plain: true });
+    res.render('single-post', {
+      ...post,
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
