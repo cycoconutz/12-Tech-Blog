82ec75acba6fecac3d41b76bc594715aac6e21ee
John Yates <johndyates@gmail.com>
2022-05-09 15:54:18 -0500

    comments adding to db

--- Develop/controllers/api/comment-routes.js
+++ Develop/controllers/api/comment-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const commentData = await Comment.findAll({
       include: [{ all: true, nested: true }],
@@ -13,7 +13,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/:id', withAuth, async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       ...req.body,
